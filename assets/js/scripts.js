7c8df032cb4ca75b46fae083c34a0b99898dfd2a
SandstormNick <gibbensnick@gmail.com>
2025-08-20 21:44:06 +0200

    Temp poc for switching theme

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,9 @@
 function toggleTheme() {
-    var currentTheme = document.body.getAttribute("data-theme");
+    var currentTheme = document.documentElement.getAttribute("data-theme");
     console.log(currentTheme);
 
-    if (currentTheme === "dark")
-        document.body.setAttribute("data-theme", "light");
-    else
-        document.body.setAttribute("data-theme", "dark");
+    var newTheme = currentTheme === "dark" ? "light" : "dark";
+
+    document.documentElement.setAttribute("data-theme", newTheme);
+    sessionStorage.setItem("theme", newTheme);
 }
